ec4a33ce5c445282e07968ac5daccec401e6a91b
E Jun <jason_e@syscom.com.cn>
2017-02-22 17:31:22 +0800

    add sqlBrite sqlDelight autovalue libraries

--- app/src/main/java/com/example/scindapsus/data/model/User.java
+++ app/src/main/java/com/example/scindapsus/data/model/User.java
@@ -1,8 +1,11 @@
 package com.example.scindapsus.data.model;
 
+import com.google.auto.value.AutoValue;
+
 /**
  * Created by ej on 2/22/2017.
  */
+@AutoValue
+public class User implements UserModel{
 
-public class User {
 }
